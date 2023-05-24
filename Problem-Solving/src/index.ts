2dee06288214ecbef7da3dcd52a5ef502b598a47
mb-tousif <meta.block.tousif@gmail.com>
2023-05-24 23:28:57 +0600

    another problem solved

--- Problem-Solving/src/index.ts
+++ Problem-Solving/src/index.ts
@@ -9,6 +9,7 @@ const compareArrays = (arr1: number[], arr2: number[]): number[] => {
 console.log(compareArrays([1, 2, 3, 4, 5], [2, 4, 6, 8, 10])); // [2, 4]
 
 // // You have an interface for Product, containing the product's id, name, price, and category. You want to filter an array of Products based on a specific criterion and value.
+// Write a TypeScript function that takes in an array of Products and a criterion string as parameters. The function should return a new array containing only the Products that match the provided criterion.
 // Answer:
 interface Product {
     id: number;
@@ -34,5 +35,34 @@ const filterProducts = (products: Product[], criterion: string): Product[] => {
 
 console.log(filterProducts(products, "Category 4")); // [{ id: 4, name: "D", price: 20, category: "Category 4" }]
 
-// Write a TypeScript function that takes in an array of Products and a criterion string as parameters. The function should return a new array containing only the Products that match the provided criterion.
+
+// Suppose you have an array of tuples, where each tuple represents a product and contains the product name, price, and quantity. Write a TypeScript function that calculates the total cost of all the products in the array, using a generic type for the tuple and a type alias for the array.
+// Answer:
+type ProductTuple = [name: string, price: number, quantity: number];
+
+const productsTpl: ProductTuple[] = [
+    ["A", 5, 10],
+    ["B", 10, 15],
+    ["C", 15, 20],
+    ["D", 20, 25],
+];
+
+// normal type
+const calculateTotal = (products: ProductTuple[]): number => {
+    const result = products.reduce((acc, curr) => acc + curr[1] * curr[2], 0);
+    return result;
+}
+
+// generic type
+function calculateTotalCost<T extends ProductTuple>(products: T[]): number {
+    let totalCost = 0;
+    for (const [name, price, quantity] of products) {
+      const productCost = price * quantity;
+      totalCost += productCost;
+    }
+    return totalCost;
+  }
+
+console.log(calculateTotal(productsTpl)); // 950
+console.log(calculateTotalCost(productsTpl)); // 950
 
