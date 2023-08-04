a781d1041e04e04ea3cfa2832c97a6d4173e606e
mb-tousif <meta.block.tousif@gmail.com>
2023-08-04 17:08:06 +0600

    SQL

---
+++ SQL/intro.sql
@@ -0,0 +1,9 @@
+CREATE TABLE buyer (
+    buyer_id INT,
+    buyer_name VARCHAR(255),
+    buyer_email VARCHAR(255)
+);
+
+INSERT INTO buyer (buyer_id, buyer_name, buyer_email)
+VALUES (1, 'John', 'john@gmail.com'),
+(2, 'Mary', 'mary@gmail.com')
