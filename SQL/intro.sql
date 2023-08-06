f2ee5888e483439919450f3930e41086d88c17d2
mb-tousif <meta.block.tousif@gmail.com>
2023-08-06 22:41:28 +0600

    pdf added

--- SQL/intro.sql
+++ SQL/intro.sql
@@ -10,3 +10,21 @@ VALUES (1, 'John', 'john@gmail.com'),
 
 -- add COLUMN
 ALTER TABLE buyer ADD COLUMN age VARCHAR(255) DEFAULT '20' not NULL;
+
+create TABLE employee (
+    employee_id SERIAL PRIMARY KEY,
+    employee_name VARCHAR(50) NOT NULL,
+    employee_email VARCHAR(255) NOT NULL
+);
+
+-- add COLUMN
+ALTER TABLE employee ADD COLUMN age INT DEFAULT 20 not NULL;
+-- DROP TABLE IF EXISTS employee;
+-- inset into employee
+INSERT INTO employee (employee_name, employee_email)
+VALUES ('John', 'phero@gmail.com'),
+('Mary', 'test@phero.com');
+-- change column CONSTRAINTS employee_age int to CHAR(255)
+ALTER TABLE employee ALTER COLUMN age TYPE CHAR(255);
+-- change column CONSTRAINTS employee_age CHAR(255) to int
+-- ALTER TABLE employee ALTER COLUMN age TYPE INT;
